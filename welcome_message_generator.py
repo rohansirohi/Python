7352407fd3ee64b876b15734b9ab0e60243dbc6a
rohansirohi <rohansirohi01@gmail.com>
2026-01-20 07:09:44 +0530

    Personalised message generator

---
+++ welcome_message_generator.py
@@ -0,0 +1,11 @@
+# Welcome Message Generator
+
+#Step 1: Ask for user details
+name = input("What's your name?")
+hobby = input("What's your favourite hobby?")
+
+# step 2: Generate a personalized welcome message
+print("\n--- Welcome Message ---")
+print(f"Hello, {name}")
+print(f"Welcome to the world of Python Programming")
+print(f"It's great to know that you love {hobby}")
