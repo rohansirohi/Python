9f8a150580ab00d1447efea4a5454bdd21825da1
rohansirohi <rohansirohi01@gmail.com>
2026-05-30 07:45:25 +0530

    Added info.py

---
+++ info.py
@@ -0,0 +1,19 @@
+# ask the user for their name
+name = input("Enter your name: ")
+
+# ask the user for their age
+age = input("Enter your age: ")
+
+# ask the user for their city
+city = input("Enter your city: ")
+
+# Display the collected information
+print("\n-----YOUR INFORMATION-----")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+# Display a greeting message
+print("\nHello", name + "!")
+print("You are", age, "years old.")
+print("You live in", city + ".")
